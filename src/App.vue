cd0b9d39f69439260e81718785d119135a356cfa
Jem Herrera <jhneramuz@gmail.com>
2022-12-12 13:48:25 -0700

    cleaned up main branch

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,14 @@
 <script setup>
-import { ref } from 'vue';
 
-const count = ref(0);
-
-function increment(){
-  count.value++;
-}
 </script>
 
 <template>
-  <h1>HELLO WORLD</h1>
-  <img src="/images/pixelpfp.jpg">
-  <button @click="increment">{{count}}</button>
+
 </template>
 
 <style lang="scss">
 @use './styles/abstracts' as *;
 @use './styles/resets.scss';
 
-h1{
-  color: $color-main;
-}
-
-button{
-  padding: 2rem;
-  background-color: $color-main;
-  color: white;
-}
 
 </style>
