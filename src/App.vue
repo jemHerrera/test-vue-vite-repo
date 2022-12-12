1567d3c49bf6539254536984fc941f80b6e922cf
Jem Herrera <jhneramuz@gmail.com>
2022-12-11 18:39:49 -0700

    setup scss

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>HELLO WORLD</h1>
+  <img src="pixelpfp.jpg">
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<style lang="scss">
+@use './styles/abstracts' as *;
+@use './styles/resets.scss';
+
+h1{
+  color: $color-main;
+  font-size: $font-size * 3;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
