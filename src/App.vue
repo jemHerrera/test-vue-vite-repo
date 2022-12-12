383ff6cb770b76f12e89f05cce9f4a8e5d70157a
Jem Herrera <jhneramuz@gmail.com>
2022-12-12 12:33:52 -0700

    added post app  component structure

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
 <script setup>
-import { ref } from 'vue';
+  import { ref, computed } from 'vue'
+  import Home from './components/views/Home.vue'
+  import NewPost from './components/views/NewPost.vue'
 
-const count = ref(0);
+  const routes = {
+    '/': Home,
+    '/new-post': NewPost
+  }
 
-function increment(){
-  count.value++;
-}
+  const currentPath = ref(window.location.hash);
+
+  window.addEventListener('hashchange', () => {
+    currentPath.value = window.location.hash;
+  })
+
+  const currentView = computed(() => {
+    return routes[currentPath.value.slice(1) || '/']
+  })
 </script>
 
 <template>
-  <h1>HELLO WORLD</h1>
-  <img src="/images/pixelpfp.jpg">
-  <button @click="increment">{{count}}</button>
+  <component :is="currerntView"></component>
 </template>
 
 <style lang="scss">
 @use './styles/abstracts' as *;
 @use './styles/resets.scss';
 
-h1{
-  color: $color-main;
-}
-
-button{
-  padding: 2rem;
-  background-color: $color-main;
-  color: white;
-}
 
 </style>
---
+++ src/components/views/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import Header from '../main-components/Header.vue'
+    import Body from '../main-components/Body.vue'
+</script>
+
+<template>
+    <Header />
+    <Body />
+    <button id="new-post-button">New Post</button>
+</template>
+
+<style scoped>
+
+</style>
